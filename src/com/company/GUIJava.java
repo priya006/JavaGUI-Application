3d4205d547d00a7b7e374a6ef227d3b8eb4690d7
pboopathi <pboopathi@ebay.com>
2020-10-03 22:43:56 -0700

    Code added to listen to the click event

--- src/com/company/GUIJava.java
+++ src/com/company/GUIJava.java
@@ -2,20 +2,24 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIJava {
-
+public class GUIJava implements ActionListener {
+    int count = 0;
+    JLabel jLabel;
     //Constructor
     public GUIJava(){
 
         JFrame frame = new JFrame();
         JPanel jPanel = new JPanel();
         JButton jButton = new JButton("Click!");
-        JLabel jLabel = new JLabel("Number of clicks");
+        jLabel = new JLabel("Number of clicks:  ");
         jPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
         jPanel.setLayout(new GridLayout(0,1));
         jPanel.add(jButton);
         jPanel.add(jLabel);
+        jButton.addActionListener(this::actionPerformed);
 
        frame.add(jPanel,BorderLayout.CENTER);
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,5 +34,11 @@ public class GUIJava {
 
         new GUIJava();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        jLabel.setText("Number of clicks" +count);
     }
 }
